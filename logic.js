27bce1208680e4d722d7cc516f7c9cc3ef529341
Sajjad Alam <84481045+alam3577@users.noreply.github.com>
2022-02-10 00:51:57 +0530

    Check If it is triangle by three sides

--- logic.js
+++ logic.js
@@ -21,4 +21,24 @@ let fabinachii = (num) => {
 let res = fabinachii(num)
 console.log("the total number",res);
 
+=============================================================================================================================================================
+
+Check If it is triangle by three sides
+
+let side1 = 2;
+let side2 = 3;
+let side3 = 4;
+
+function checkItIsTraingle(side1, side2, side3) {
+    if(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1){
+        return true;
+    }
+    return false;
+}
+
+let res = checkItIsTraingle(side1, side2, side3);
+
+console.log(res);
+
+==============================================================================================================================================================
 */
