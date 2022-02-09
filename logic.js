8af4ac89b21584c1b1d59af4159ec904362e55b4
Sajjad Alam <84481045+alam3577@users.noreply.github.com>
2022-02-10 00:32:35 +0530

    fabonacci series

---
+++ logic.js
@@ -0,0 +1,24 @@
+/*
+// Fabonacci Series
+
+let num = 10;
+
+let fabinachii = (num) => {
+  let a = -1;
+  let b = 1;
+  let c;
+  let sum = 0
+  for (let i = 1; i < 10; i++) {
+       console.log("the fabinachhi number",c);     
+     c = a +b 
+     a = b
+     b = c  
+     sum+= c
+  }
+  return sum
+}
+
+let res = fabinachii(num)
+console.log("the total number",res);
+
+*/
